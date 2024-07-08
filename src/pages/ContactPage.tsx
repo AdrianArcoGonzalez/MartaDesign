0a651a6a7b2a64f0a73b502ed1e8bf76502de2c3
Adrian Arco Gonzalez <adrian.arco.gonzalez@gmail.com>
2024-07-08 21:55:20 +0200

    Add info

--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -5,6 +5,17 @@ const ContactPage = () => {
   return (
     <ContactPageStyled>
       <Header />
+      <section className="dataSection">
+        <article>
+          <p className="text">
+            Quieres saber más sobre mis trabajos? Envíame un correo electrónico
+          </p>
+          <p className="text">Envíame un correo electrónico</p>
+        </article>
+        <article>
+          <span className="text">calvofernandezmarta@gmail.com</span>
+        </article>
+      </section>
     </ContactPageStyled>
   );
 };
@@ -12,12 +23,27 @@ const ContactPage = () => {
 const ContactPageStyled = styled.div`
   display: flex;
   padding-top: 40px;
-  justify-content: flex-start;
+  flex-direction: column;
+  justify-content: center;
   align-items: center;
   gap: 20px;
   height: 100%;
   width: 100%;
   margin-bottom: 40px;
-  flex-direction: column;
+
+  .dataSection {
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+    justify-content: center;
+    align-items: center;
+  }
+  .text {
+    margin: 0;
+    padding: 10px;
+    text-align: center;
+    color: "#00000";
+    font-weight: 500;
+  }
 `;
 export default ContactPage;
